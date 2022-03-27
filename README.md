9a95b4e36dc1611abdbdfad1890ddd1642d8e349
j-dunham <jmdunham20@gmail.com>
2022-03-27 09:19:30 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# *GRit*
+Simple implementation of `git` using ruby.  Starting code and inspiration from [here](https://thoughtbot.com/blog/rebuilding-git-in-ruby)
---
+++ lib/grit/object.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'fileutils'
+
+module GRit
+  GRIT_DIRECTORY = "#{Dir.pwd}/,grit"
+  OBJECTS_DIRECTORY = "#{GRIT_DIRECTORY}/objects"
+  REFS_DIRECTORY = "#{GRIT_DIRECTORY}/refs"
+  INDEX_PATH = "#{GRIT_DIRECTORY}/index"
+
+  class Object
+    attr_reader :sha
+
+    def initialize(sha)
+      @sha = sha
+    end
+
+    def write(&block)
+      object_directory = "#{OBJECTS_DIRECTORY}/#{sha[0..1]}"
+      FileUtils.mkdir_p object_directory
+      object_path = "#{object_directory}/#{sha[2..-1]}"
+      File.open(object_path, 'w', &block)
+    end
+  end
+end
